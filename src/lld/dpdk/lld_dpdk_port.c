399d516ba23c001a3e151aae72d0e01da7fdaabd
Vemuri Hari Priya <hari.priya.vemuri@intel.com>
2022-07-17 22:14:21 -0400

    Fixed DPDK Klocwork issues -severity Error List of issues: https://klocwork-jf19.devtools.intel.com:8165/review/insight-review.html#issuelist_goto:project=NDvSwitch_P4_DPDK_TARGET,searchquery=Status%253AFix+in+Next+Release+severity%253AError,sortcolumn=id,sortdirection=ASC,start=0,view_id=1

--- src/lld/dpdk/lld_dpdk_port.c
+++ src/lld/dpdk/lld_dpdk_port.c
@@ -75,7 +75,7 @@ int lld_dpdk_pipeline_tap_port_add(bf_dev_port_t dev_port,
 		if (sfd == -1)
 			LOG_ERROR("Socket creation failed\n");
 		memset(&ifr, 0, sizeof(ifr));
-		strncpy(ifr.ifr_name, port_attrib->port_name, sizeof(ifr.ifr_name));
+		strncpy(ifr.ifr_name, port_attrib->port_name, sizeof(ifr.ifr_name) - 1);
 		ifr.ifr_mtu = port_attrib->tap.mtu;
 		if (ioctl(sfd, SIOCSIFMTU, &ifr) < 0)
 			LOG_ERROR("ioctl SIOCSIFMTU Failed...\n");
